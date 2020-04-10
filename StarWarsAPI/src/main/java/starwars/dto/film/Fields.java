73248b4748d487f20a3e9958255dd3aa91be88d2
TanskiV <tanskiyv@gmail.com>
2020-04-10 18:32:53 +0300

    v1

--- StarWarsAPI/src/main/java/starwars/dto/film/Fields.java
+++ StarWarsAPI/src/main/java/starwars/dto/film/Fields.java
@@ -2,6 +2,13 @@ package starwars.dto.film;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Fields {
 	List<Integer> starships;
 	String edited;
